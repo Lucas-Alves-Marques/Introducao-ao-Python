310813768f89fa17da80b19e2cdeccb6764416ba
Lucas Alves <lucas.marquesalv24@gmail>
2024-10-21 21:09:21 -0300

    Adição das Atividades em Python

---
+++ Atividade.py
@@ -0,0 +1,39 @@
+# lista_compras = ['banana','laranja','maçã']
+# print(lista_compras)
+# print(lista_compras[1])
+# print(lista_compras[2])
+# #print(lista_compras[3]) #mostra erro pois ele começar a contar da posição zero e não possui posição 3
+# print(lista_compras[-3]) #mostra o item do ultimo posição para o começo
+# lista_compras.append('goiaba') #insere novo item no final da lista
+# print(lista_compras)
+# lista_compras.insert(1,'ameixa') #insere um novo item na posição 1
+# print(lista_compras)
+# del(lista_compras[3]) #vai deletar o item na posição 3
+# print(lista_compras)
+# lista_compras.remove('laranja') #usa-se o remove quando não se sabe a posição do item
+# print(lista_compras)
+
+
+# #slide4 - trocando itens na lista
+
+# lista=[1,2,3]
+# print(lista)
+# lista[0]=4
+# print(lista)
+
+# tupla = (1,2,3)
+# print(tupla)
+# tupla[0]=4
+# print(tupla)
+
+
+programadores = ['Victor', 'Juliana', 'Caio', 'Luana','Samuel']
+print(type(programadores))
+print(len(programadores))
+print(programadores[4])
+
+# programadores = ('Victor', 'Juliana', 'Caio', 'Luana','Samuel')
+# print(type(programadores))
+# print(len(programadores))
+# print(programadores[4])
+
---
+++ Atividade_08-10.py
@@ -0,0 +1,85 @@
+                                        # Exercicio 1
+
+# numeros = [1,2,3,4,5,6,7,8,9,10]
+
+# print(numeros)
+
+
+                                        # Exercicio 2
+
+#numerosInt = [1,2,3,4,5,6,7,8,9,10]
+
+#print(sum(numerosInt))
+
+                                        # Execicio 3
+
+# frutas = (["pera","abacaxi","laranja","uva"])
+
+# fruta_procurada = "morango"
+
+# if fruta_procurada in frutas:
+#     print("A fruta esta na lista")
+
+# else:
+#     print("A fruta não está na lista")
+
+                                        # Exercicio 4
+
+#numeros = [1,4,2,5,6,3,8,4,2,6,8,3,3]
+
+#print(numeros.count(3))
+
+                                        # Exercicio 5
+
+# nomes = ["Ana","Burno","Carlos","Daniela"]
+
+# nomes.remove("Carlos")
+
+# print(nomes)
+
+                                        # Execicios 6
+
+
+# lista1 =[1,2,3]
+
+# lista2 = [4,5,6]
+
+# lista_completa = lista1 + lista2
+
+# print(lista_completa)
+
+                                        # Exercicio 7
+
+
+# frutas = ["banana", "maça", "pera", "uva","melancia","melao"]
+
+# frutas.append("pitanga")
+# frutas.append("caju")
+
+# print(frutas)
+
+                                        # Exercicio 8
+
+# frutas = ["banana", "maça", "pera", "uva","melancia"]
+
+# frutas[1] = "caju"
+
+# print(frutas)
+
+                                       # Exercicio 9
+
+# frutas = ["banana", "maça", "pera", "uva","melancia","melao","pitanga","caju"]
+
+# frutas.insert(1,"manga")
+
+# print(frutas)
+
+
+                                    # Execicio 10
+
+# frutas = ["banana", "maça", "pera", "uva","melancia"]
+
+# del(frutas[3])
+
+# print(frutas)
+
